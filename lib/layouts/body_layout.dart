9056b0a38d5920e5b9ac4b781baffd3e561a5cd0
Matias Ramirez <ramirezmatias946@gmail.com>
2024-03-31 17:41:57 -0400

    Add BodyLayout and export it in layouts.dart

---
+++ lib/layouts/body_layout.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+import 'package:my_portfolio/extensions/extensions.dart';
+
+class BodyLayout extends StatelessWidget {
+  const BodyLayout({
+    super.key,
+    required this.childrenForDesktop,
+    required this.childrenForMobile,
+  });
+
+  final List<Widget> childrenForMobile;
+  final List<Widget> childrenForDesktop;
+
+  @override
+  Widget build(BuildContext context) {
+    final isMobileSize = MediaQuery.of(context).isMobileSize;
+
+    if (isMobileSize) {
+      return Column(
+        children: childrenForMobile,
+      );
+    }
+
+    return SizedBox(
+      height: 500,
+      child: Row(
+        children: childrenForDesktop,
+      ),
+    );
+  }
+}
