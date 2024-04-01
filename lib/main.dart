cc64ba32e980b810ae0660e66547a0114497cbc3
Matias Ramirez <ramirezmatias946@gmail.com>
2024-03-31 23:00:23 -0400

    Add routes and pages for experience, education, and projects

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,16 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:go_router/go_router.dart';
+import 'package:my_portfolio/layouts/layouts.dart';
+import 'package:my_portfolio/pages/education_page.dart';
+import 'package:my_portfolio/pages/experience_page.dart';
 import 'package:my_portfolio/pages/home_page.dart';
+import 'package:my_portfolio/pages/projects_page.dart';
 import 'package:my_portfolio/providers/providers.dart';
 import 'package:my_portfolio/themes/themes.dart';
+
+final _rootNavigatorKey = GlobalKey<NavigatorState>();
+final _shellNavigatorKey = GlobalKey<NavigatorState>();
 
 void main() => runApp(ProviderScope(child: MyApp()));
 
@@ -11,11 +18,34 @@ class MyApp extends ConsumerWidget {
   MyApp({super.key});
 
   final _router = GoRouter(
+    navigatorKey: _rootNavigatorKey,
     initialLocation: '/',
     routes: [
-      GoRoute(
-        path: '/',
-        builder: (context, state) => HomePage(),
+      ShellRoute(
+        navigatorKey: _shellNavigatorKey,
+        builder: (context, state, child) => PageLayout(body: child),
+        routes: [
+          GoRoute(
+            path: '/',
+            name: 'home',
+            builder: (context, state) => const HomePage(),
+          ),
+          GoRoute(
+            path: '/experience',
+            name: 'experience',
+            builder: (context, state) => const ExperiencePage(),
+          ),
+          GoRoute(
+            path: '/education',
+            name: 'education',
+            builder: (context, state) => const EducationPage(),
+          ),
+          GoRoute(
+            path: '/projects',
+            name: 'projects',
+            builder: (context, state) => const ProjectsPage(),
+          ),
+        ],
       ),
     ],
   );
