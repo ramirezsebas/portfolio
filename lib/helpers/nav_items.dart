8c7065b9eb1bccf514356dbfca74c1154f9b7709
Matias Ramirez <ramirezmatias946@gmail.com>
2024-03-31 23:00:54 -0400

    Update navItems in nav_items.dart

--- lib/helpers/nav_items.dart
+++ lib/helpers/nav_items.dart
@@ -1,19 +1,31 @@
 import 'package:flutter/material.dart';
 
 class NavItem {
-  final Icon icon;
-  final String label;
-
   const NavItem({
     required this.icon,
     required this.label,
+    required this.pathName,
   });
+
+  final Icon icon;
+  final String label;
+  final String pathName;
 }
 
 const navItems = [
-  NavItem(icon: Icon(Icons.bookmark_rounded), label: "Skills"),
-  NavItem(icon: Icon(Icons.workspaces_rounded), label: "Projects"),
-  NavItem(icon: Icon(Icons.work_rounded), label: "Experience"),
-  NavItem(icon: Icon(Icons.school_rounded), label: "Education"),
-  NavItem(icon: Icon(Icons.file_copy_rounded), label: "Resumé"),
+  NavItem(
+    icon: Icon(Icons.work_rounded),
+    label: "Projects",
+    pathName: 'projects',
+  ),
+  NavItem(
+    icon: Icon(Icons.work_rounded),
+    label: "Experience",
+    pathName: 'experience',
+  ),
+  NavItem(
+    icon: Icon(Icons.school_rounded),
+    label: "Education",
+    pathName: 'education',
+  ),
 ];
