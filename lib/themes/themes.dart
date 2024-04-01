e3023f00ced599a6d10a5644fa68e5d0a2add8e3
Matias Ramirez <ramirezmatias946@gmail.com>
2024-03-31 21:45:57 -0400

    Add Google Fonts and update theme settings

--- lib/themes/themes.dart
+++ lib/themes/themes.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 ThemeData lightTheme = ThemeData(
   brightness: Brightness.light,
+  fontFamily: GoogleFonts.lato().fontFamily,
   useMaterial3: true,
   textTheme: const TextTheme(
     displayLarge: TextStyle(fontSize: 32, fontWeight: FontWeight.bold),
@@ -15,6 +17,8 @@ ThemeData lightTheme = ThemeData(
 );
 
 ThemeData darkTheme = ThemeData(
+  useMaterial3: true,
+  fontFamily: GoogleFonts.lato().fontFamily,
   textTheme: const TextTheme(
     displayLarge: TextStyle(fontSize: 32, fontWeight: FontWeight.bold),
     bodyLarge: TextStyle(fontSize: 18, color: Colors.white70),
