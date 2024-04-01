b75f877ce7607550d64920c8ba62429ad2602e7b
Matias Ramirez <ramirezmatias946@gmail.com>
2024-03-31 23:19:37 -0400

    Remove outdated experience cards

--- lib/pages/experience_page.dart
+++ lib/pages/experience_page.dart
@@ -13,20 +13,6 @@ class ExperiencePage extends StatelessWidget {
           style: Theme.of(context).textTheme.displayLarge,
         ),
         const SizedBox(height: 20),
-        const CustomCard(
-          title: "Software Engineer",
-          companyName: "ITTI Digital",
-          duration: "May 2022 - Present",
-          bio:
-              '''I contribute to fintech projects like "Ueno," using Flutter/React, ensuring robustness and scalability in app development.''',
-        ),
-        const CustomCard(
-          title: "Software Engineer",
-          companyName: "Kernel SRL",
-          duration: "Febuary 2021 - May 2022",
-          bio:
-              '''I support, maintain, and enhance RESTful APIs in Node.js, migrate to TypeScript, deploy services, and develop applications in Flutter and Angular.''',
-        ),
       ],
     );
   }
