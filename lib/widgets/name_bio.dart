fb85b407093a92a808cc100a750668b11f62a8d8
Matias Ramirez <ramirezmatias946@gmail.com>
2024-04-02 18:00:53 -0400

    Add social media buttons to NameBio widget

--- lib/widgets/name_bio.dart
+++ lib/widgets/name_bio.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:my_portfolio/extensions/extensions.dart';
+import 'package:my_portfolio/models/social_media.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class NameBio extends StatelessWidget {
   const NameBio({
@@ -14,6 +16,7 @@ class NameBio extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final isMobileSize = MediaQuery.of(context).isMobileSize;
+    final isDarkMode = Theme.of(context).brightness == Brightness.dark;
 
     return Column(
       mainAxisAlignment: MainAxisAlignment.center,
@@ -31,6 +34,40 @@ class NameBio extends StatelessWidget {
           bio,
           style: Theme.of(context).textTheme.bodyLarge,
           textAlign: !isMobileSize ? TextAlign.start : TextAlign.center,
+        ),
+        const SizedBox(
+          height: 10,
+        ),
+        Wrap(
+          spacing: 10,
+          children: socialMedias
+              .map(
+                (skill) => ElevatedButton.icon(
+                  onPressed: () {
+                    final uri = Uri.parse(skill.url);
+                    launchUrl(uri)
+                        .then((value) => debugPrint("Launched ${skill.name}"))
+                        .catchError((error) => debugPrint(
+                            "Error launching ${skill.name}: $error"));
+                  },
+                  icon: Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: CircleAvatar(
+                      backgroundColor: Colors.transparent,
+                      child: Image.asset(
+                        skill.icon,
+                        color: isDarkMode
+                            ? Colors.white
+                            : isDarkMode
+                                ? Colors.black
+                                : null,
+                      ),
+                    ),
+                  ),
+                  label: Text(skill.name),
+                ),
+              )
+              .toList(),
         ),
       ],
     );
