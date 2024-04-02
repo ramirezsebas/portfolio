a27f59526270ab6ad99a94d38773a84a17159b0c
Matias Ramirez <ramirezmatias946@gmail.com>
2024-04-01 22:52:05 -0400

    Add size-based device detection extensions

--- lib/extensions/extensions.dart
+++ lib/extensions/extensions.dart
@@ -3,6 +3,10 @@ import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 
 extension SizeX on MediaQueryData {
   bool get isMobileSize => size.width < 850;
+
+  bool get isTabletSize => size.width < 1100 && size.width >= 850;
+
+  bool get isDesktopSize => size.width >= 1100;
 }
 
 extension AppLocalizationsX on BuildContext {
