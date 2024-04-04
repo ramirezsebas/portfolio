2e4e0eded5400b5a3dfb4d631451228d03210bb1
Matias Ramirez <ramirezmatias946@gmail.com>
2024-04-03 22:13:51 -0400

    Add SkillSection widget to SkillsPage

--- lib/pages/skills_page.dart
+++ lib/pages/skills_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:my_portfolio/models/skills.dart';
+import 'package:my_portfolio/widgets/skill_section.dart';
 
 class SkillsPage extends StatelessWidget {
   const SkillsPage({super.key});
@@ -31,67 +32,6 @@ class SkillsPage extends StatelessWidget {
           ),
         ],
       ),
-    );
-  }
-}
-
-class SkillSection extends StatelessWidget {
-  const SkillSection({
-    super.key,
-    required this.skillCategory,
-  });
-
-  final SkillCategory skillCategory;
-
-  @override
-  Widget build(BuildContext context) {
-    return Column(
-      children: [
-        Row(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            Text(
-              skillCategory.name,
-              style: Theme.of(context).textTheme.displaySmall,
-            ),
-            const SizedBox(width: 20),
-            const Expanded(
-              child: Divider(
-                thickness: 1,
-              ),
-            ),
-          ],
-        ),
-        const SizedBox(height: 20),
-        SizedBox(
-          width: MediaQuery.of(context).size.width,
-          child: Wrap(
-            spacing: 20,
-            children: skillCategory.skills
-                .map(
-                  (e) => SizedBox(
-                    width: 200,
-                    child: Card(
-                      child: Padding(
-                        padding: const EdgeInsets.all(12.0),
-                        child: Row(
-                          children: [
-                            Text(e.name),
-                            const Spacer(),
-                            Image.asset(
-                              e.icon,
-                              height: 40,
-                            ),
-                          ],
-                        ),
-                      ),
-                    ),
-                  ),
-                )
-                .toList(),
-          ),
-        ),
-      ],
     );
   }
 }
---
+++ lib/widgets/skill_section.dart
@@ -0,0 +1,63 @@
+import 'package:flutter/material.dart';
+import 'package:my_portfolio/models/skills.dart';
+
+class SkillSection extends StatelessWidget {
+  const SkillSection({
+    super.key,
+    required this.skillCategory,
+  });
+
+  final SkillCategory skillCategory;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Row(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Text(
+              skillCategory.name,
+              style: Theme.of(context).textTheme.displaySmall,
+            ),
+            const SizedBox(width: 20),
+            const Expanded(
+              child: Divider(
+                thickness: 1,
+              ),
+            ),
+          ],
+        ),
+        const SizedBox(height: 20),
+        SizedBox(
+          width: MediaQuery.of(context).size.width,
+          child: Wrap(
+            spacing: 20,
+            children: skillCategory.skills
+                .map(
+                  (e) => SizedBox(
+                    width: 200,
+                    child: Card(
+                      child: Padding(
+                        padding: const EdgeInsets.all(12.0),
+                        child: Row(
+                          children: [
+                            Text(e.name),
+                            const Spacer(),
+                            Image.asset(
+                              e.icon,
+                              height: 40,
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                  ),
+                )
+                .toList(),
+          ),
+        ),
+      ],
+    );
+  }
+}
