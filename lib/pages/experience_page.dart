152cf93fd6d1fbf6e8c1a7e27690fad866e879c9
Matias Ramirez <ramirezmatias946@gmail.com>
2024-04-04 07:41:52 -0400

    Commented out GridView.builder code in ExperiencePage

--- lib/pages/experience_page.dart
+++ lib/pages/experience_page.dart
@@ -94,54 +94,6 @@ class ExperiencePage extends ConsumerWidget {
                   ],
                 ),
               ),
-          // GridView.builder(
-          //   gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-          //     crossAxisCount: 3,
-          //     crossAxisSpacing: 10,
-          //     mainAxisSpacing: 10,
-          //   ),
-          //   itemCount: experiences.length,
-          //   shrinkWrap: true,
-          //   itemBuilder: (context, index) {
-          //     final experience = experiences[index];
-          //     return Card(
-          //       child: Padding(
-          //         padding: const EdgeInsets.all(8.0),
-          //         child: Column(
-          //           mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          //           children: [
-          //             Text(
-          //               experience.title,
-          //               style: Theme.of(context).textTheme.headlineSmall,
-          //               textAlign: TextAlign.center,
-          //             ),
-          //             Text(
-          //               experience.description,
-          //               textAlign: TextAlign.center,
-          //               style: Theme.of(context).textTheme.bodyMedium,
-          //             ),
-          //             Text(
-          //               experience.techStack,
-          //               textAlign: TextAlign.center,
-          //               style: Theme.of(context).textTheme.bodySmall!.copyWith(
-          //                     color: Colors
-          //                         .primaries[index % Colors.primaries.length],
-          //                   ),
-          //             ),
-          //             Text(
-          //               experience.date,
-          //               textAlign: TextAlign.center,
-          //               style: Theme.of(context).textTheme.bodySmall!.copyWith(
-          //                     color: Colors
-          //                         .primaries[index % Colors.primaries.length],
-          //                   ),
-          //             ),
-          //           ],
-          //         ),
-          //       ),
-          //     );
-          //   },
-          // ),
         ],
       ),
     );
