f6907d75f50ab2446d07bafaeb14babfd2900353
Matias Ramirez <ramirezmatias946@gmail.com>
2024-04-03 22:10:20 -0400

    Add new icons

--- lib/gen/assets.gen.dart
+++ lib/gen/assets.gen.dart
@@ -12,28 +12,77 @@ import 'package:flutter/widgets.dart';
 class $AssetsIconsGen {
   const $AssetsIconsGen();
 
+  /// File path: assets/icons/angular.png
+  AssetGenImage get angular => const AssetGenImage('assets/icons/angular.png');
+
   /// File path: assets/icons/arroba.png
   AssetGenImage get arroba => const AssetGenImage('assets/icons/arroba.png');
 
+  /// File path: assets/icons/dart.png
+  AssetGenImage get dart => const AssetGenImage('assets/icons/dart.png');
+
   /// File path: assets/icons/docker.png
   AssetGenImage get docker => const AssetGenImage('assets/icons/docker.png');
+
+  /// File path: assets/icons/express.png
+  AssetGenImage get express => const AssetGenImage('assets/icons/express.png');
 
   /// File path: assets/icons/facebook.png
   AssetGenImage get facebook =>
       const AssetGenImage('assets/icons/facebook.png');
 
+  /// File path: assets/icons/fastify.png
+  AssetGenImage get fastify => const AssetGenImage('assets/icons/fastify.png');
+
+  /// File path: assets/icons/flutter.png
+  AssetGenImage get flutter => const AssetGenImage('assets/icons/flutter.png');
+
+  /// File path: assets/icons/git.png
+  AssetGenImage get git => const AssetGenImage('assets/icons/git.png');
+
   /// File path: assets/icons/github.png
   AssetGenImage get github => const AssetGenImage('assets/icons/github.png');
 
+  /// File path: assets/icons/gitlab.png
+  AssetGenImage get gitlab => const AssetGenImage('assets/icons/gitlab.png');
+
+  /// File path: assets/icons/js.jpg
+  AssetGenImage get jsJpg => const AssetGenImage('assets/icons/js.jpg');
+
   /// File path: assets/icons/js.png
-  AssetGenImage get js => const AssetGenImage('assets/icons/js.png');
+  AssetGenImage get jsPng => const AssetGenImage('assets/icons/js.png');
 
   /// File path: assets/icons/linkedin.png
   AssetGenImage get linkedin =>
       const AssetGenImage('assets/icons/linkedin.png');
 
+  /// File path: assets/icons/linux.png
+  AssetGenImage get linux => const AssetGenImage('assets/icons/linux.png');
+
+  /// File path: assets/icons/macos.png
+  AssetGenImage get macos => const AssetGenImage('assets/icons/macos.png');
+
+  /// File path: assets/icons/mongodb.png
+  AssetGenImage get mongodb => const AssetGenImage('assets/icons/mongodb.png');
+
+  /// File path: assets/icons/mysql.png
+  AssetGenImage get mysql => const AssetGenImage('assets/icons/mysql.png');
+
   /// File path: assets/icons/nodejs.png
   AssetGenImage get nodejs => const AssetGenImage('assets/icons/nodejs.png');
+
+  /// File path: assets/icons/npm.png
+  AssetGenImage get npm => const AssetGenImage('assets/icons/npm.png');
+
+  /// File path: assets/icons/postgresql.png
+  AssetGenImage get postgresql =>
+      const AssetGenImage('assets/icons/postgresql.png');
+
+  /// File path: assets/icons/postman.png
+  AssetGenImage get postman => const AssetGenImage('assets/icons/postman.png');
+
+  /// File path: assets/icons/react.png
+  AssetGenImage get react => const AssetGenImage('assets/icons/react.png');
 
   /// File path: assets/icons/twitter.png
   AssetGenImage get twitter => const AssetGenImage('assets/icons/twitter.png');
@@ -42,17 +91,45 @@ class $AssetsIconsGen {
   AssetGenImage get typescript =>
       const AssetGenImage('assets/icons/typescript.png');
 
+  /// File path: assets/icons/vscode.png
+  AssetGenImage get vscode => const AssetGenImage('assets/icons/vscode.png');
+
+  /// File path: assets/icons/windows.png
+  AssetGenImage get windows => const AssetGenImage('assets/icons/windows.png');
+
+  /// File path: assets/icons/yarn.png
+  AssetGenImage get yarn => const AssetGenImage('assets/icons/yarn.png');
+
   /// List of all assets
   List<AssetGenImage> get values => [
+        angular,
         arroba,
+        dart,
         docker,
+        express,
         facebook,
+        fastify,
+        flutter,
+        git,
         github,
-        js,
+        gitlab,
+        jsJpg,
+        jsPng,
         linkedin,
+        linux,
+        macos,
+        mongodb,
+        mysql,
         nodejs,
+        npm,
+        postgresql,
+        postman,
+        react,
         twitter,
-        typescript
+        typescript,
+        vscode,
+        windows,
+        yarn
       ];
 }
 
